ff6590ed65c51108daf557ca77ba1f1ebf382bec
kkdusj <104974463+kkdusj@users.noreply.github.com>
2022-05-24 12:42:43 +0200

    Add files via upload

--- Fast.lua
+++ Fast.lua
@@ -13291,34 +13291,6 @@ name = string.gsub(name,"جسر","ر ج س")
 return LuaTele.sendText(msg_chat_id,msg_id,"⋆ اسرع واحد يرتبها ~ {"..name.."}","md",true)  
 end
 end
-if text == "تفعيل سمسمي" then
-if not msg.Manger then
-return send(msg_chat_id,msg_id,'\n*❍ هذا الامر يخص  '..Controller_Num(6)..' * ',"md",true)  
-end
-Redis:del(Fast.."smsme"..msg.chat_id)
-return send(msg.chat_id,msg.id,"❍ تم تفعيل سمسمي")
-end
-if text == "تعطيل سمسمي" then
-if not msg.Manger then
-return send(msg_chat_id,msg_id,'\n*❍ هذا الامر يخص  '..Controller_Num(6)..' * ',"md",true)  
-end
-Redis:set(Fast.."smsme"..msg.chat_id,true)
-return send(msg.chat_id,msg.id,"❍ تم تعطيل سمسمي")
-end
-if not Redis:get(Fast.."smsme"..msg.chat_id) then
-if text and msg.reply_to_message_id ~= 0 then
-local Message_Reply = LuaTele.getMessage(msg.chat_id, msg.reply_to_message_id)
-if Message_Reply and Message_Reply.sender and tonumber(Message_Reply.sender.user_id) == tonumber(Fast) then
-ai_text = https.request("https://ayad-12.xyz/sm.php?text="..URL.escape(text))
-if ai_text:match("(.*)سناب(.*)") or ai_text:match("(.*)واتس(.*)") or ai_text:match("(.*)انستا(.*)") or ai_text:match("(.*)رقمي(.*)") or ai_text:match("(%d+)") or ai_text:match("(.*)متابعه(.*)") or ai_text:match("(.*)تابعني(.*)") or ai_text:match("(.*)قناتي(.*)") or ai_text:match("(.*)قناه(.*)") or ai_text:match("(.*)يوتيوب(.*)") then
-txx = "لا افهمك"
-else
-txx = ai_text
-end
-send(msg_chat_id,msg_id,txx,"md")
-end
-end
-end
 if text == "خيروك" or text == "لو خيروك" then
 if Redis:get(Fast.."Status:Games"..msg.chat_id) then
 local texting = {
@@ -14028,7 +14000,34 @@ return LuaTele.sendText(msg_chat_id,msg_id,[[*
 *]],"md")
 end
 end -- GroupBot
-
+if text == "تفعيل سمسمي" then
+if not msg.Manger then
+return send(msg_chat_id,msg_id,'\n*❍ هذا الامر يخص  '..Controller_Num(6)..' * ',"md",true)  
+end
+Redis:del(Fast.."smsme"..msg.chat_id)
+send(msg.chat_id,msg.id,"❍ تم تفعيل سمسمي")
+end
+if text == "تعطيل سمسمي" then
+if not msg.Manger then
+return send(msg_chat_id,msg_id,'\n*❍ هذا الامر يخص  '..Controller_Num(6)..' * ',"md",true)  
+end
+Redis:set(Fast.."smsme"..msg.chat_id,true)
+send(msg.chat_id,msg.id,"❍ تم تعطيل سمسمي")
+end
+if not Redis:get(Fast.."smsme"..msg.chat_id) then
+if text and msg.reply_to_message_id ~= 0 then
+local Message_Reply = LuaTele.getMessage(msg.chat_id, msg.reply_to_message_id)
+if Message_Reply and Message_Reply.sender and tonumber(Message_Reply.sender.user_id) == tonumber(Fast) then
+ai_text = https.request("https://ayad-12.xyz/sm.php?text="..URL.escape(text))
+if ai_text:match("(.*)سناب(.*)") or ai_text:match("(.*)واتس(.*)") or ai_text:match("(.*)انستا(.*)") or ai_text:match("(.*)رقمي(.*)") or ai_text:match("(%d+)") or ai_text:match("(.*)متابعه(.*)") or ai_text:match("(.*)تابعني(.*)") or ai_text:match("(.*)قناتي(.*)") or ai_text:match("(.*)قناه(.*)") or ai_text:match("(.*)يوتيوب(.*)") then
+txx = "لا افهمك"
+else
+txx = ai_text
+end
+send(msg_chat_id,msg_id,txx,"md")
+end
+end
+end
 
 if chat_type(msg.chat_id) == "UserBot" then 
 if text == 'تحديث الملفات ⋆' or text == 'تحديث' then
